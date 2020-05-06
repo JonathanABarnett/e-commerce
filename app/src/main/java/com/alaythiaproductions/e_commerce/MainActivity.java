9ab0ca1eecd3d37b9eb50dfa72ba4bd610345399
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-05-06 16:54:22 -0500

    Add main page.

--- app/src/main/java/com/alaythiaproductions/e_commerce/MainActivity.java
+++ app/src/main/java/com/alaythiaproductions/e_commerce/MainActivity.java
@@ -3,12 +3,18 @@ package com.alaythiaproductions.e_commerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signUpButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUpButton = findViewById(R.id.main_sign_up_btn);
+        loginButton = findViewById(R.id.main_login_btn);
     }
 }
