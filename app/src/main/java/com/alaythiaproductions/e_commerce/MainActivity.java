33df7f8e0554a8f4fba4b532416941c945651340
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-05-06 22:11:02 -0500

    Add Login Page.

--- app/src/main/java/com/alaythiaproductions/e_commerce/MainActivity.java
+++ app/src/main/java/com/alaythiaproductions/e_commerce/MainActivity.java
@@ -2,7 +2,9 @@ package com.alaythiaproductions.e_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
         signUpButton = findViewById(R.id.main_sign_up_btn);
         loginButton = findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
